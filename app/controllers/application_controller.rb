04daee10560417b9c92e64a0f4aaeb68352b7616
Andres Bravo <andresbravog@gmail.com>
2015-06-20 22:09:50 +0200

    Add some application controller helpers and root action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,25 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # GET /
+  def index
+  end
+
+  private
+
+  # Returns current user if exists
+  #
+  # @retuns [User]
+  def current_user
+    return nil unless session[:username].present?
+    @current_user ||= User.new(session)
+  end
+
+  # Checks current user or redirect to login path
+  #
+  def authenticate_user!
+    return if current_user.present?
+    redirect_to login_path
+  end
 end
