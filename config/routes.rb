021ea10eab6e4d6eed4db38d7ec0b91e54500759
Andres Bravo <andresbravog@gmail.com>
2015-06-20 22:10:58 +0200

    Add session and root routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'application#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -53,4 +53,8 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  get 'login'            => 'sessions#new',     as: :login
+  delete 'logout'        => 'sessions#destroy', as: :logout
+  post 'sessions/create' => 'sessions#create',  as: :create_session
 end
