cb9de9c57095ff17cca53fb700c84de40b9bc26c
Andres Bravo <andresbravog@gmail.com>
2015-07-01 11:31:31 +0200

    added valley girl translate

--- lib/translate/backend/valley_girl.rb
+++ lib/translate/backend/valley_girl.rb
@@ -1,5 +1,9 @@
 module Translate::Backend::ValleyGirl
+  API_URL = 'http://www.degraeve.com/cgi-bin/babel.cgi'
+  
   def self.translate(message)
-    'My cousin is rich: ' +  message
+    response = RestClient.get API_URL, params: { d: 'valley', w: message }
+    parser = Nokogiri::HTML(response.body)
+    parser.xpath('//blockquote').map(&:content).first
   end
 end
