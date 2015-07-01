01c4a504096f921de87d257935d76f8c3cb4252e
Andres Bravo <andresbravog@gmail.com>
2015-07-01 09:18:39 +0200

    Start palying with faye subscriptions

--- app/realtime/realtime_messages_controller.rb
+++ app/realtime/realtime_messages_controller.rb
@@ -1,5 +1,11 @@
 class RealtimeMessagesController < FayeRails::Controller
   channel '/messages' do
+    monitor :subscribe do
+      puts "Client #{client_id} subscribed to #{channel}. #{inspect}"
+    end
+    monitor :unsubscribe do
+      puts "Client #{client_id} unsubscribed from #{channel}."
+    end
     subscribe do
       Rails.logger.debug "Received on #{channel}: #{inspect}"
     end
